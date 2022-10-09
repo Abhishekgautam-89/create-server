dafb41d0e6d789ba55cf9b2080fc49f4e34475da
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-10-09 23:57:15 +0530

    creating different url's in local host

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log('Abhishek Gautam');
-    
+    // console.log('Abhishek Gautam');
+    if (req.url==='/home'){
+        res.write('<html><body><h1>Welcome To Home</h1></body></html>');
+    }
+    else if (req.url==='/about'){
+        res.write('<html><body><h1>Welcome To About us Page</h1></body></html>');
+    }
+    else if(req.url==='/node'){ 
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>My First HTML Page</title></head>')
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    res.write('</html>')
+    }    
+    res.end();
 })
 
 server.listen(4000);
